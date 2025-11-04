72c5eb3681a80844cdf2fe1b5ab81d61eb9fc84c
IrumShehryar <irum.shehryar@gmail.com>
2025-11-04 17:12:42 +0200

    Connected to database

---
+++ backend/api/utils/db.py
@@ -0,0 +1,13 @@
+from mongoengine import connect
+import os
+
+def mongo_connect():
+    try:
+        connection = connect(
+            db=os.getenv("DB_NAME"),
+            host=os.getenv("DATABASE_URL")
+        )
+        print("DB connected successfully")
+        return connection
+    except Exception as e:
+        print("Connection to db failed:", str(e))
